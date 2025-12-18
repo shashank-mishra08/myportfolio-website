6f6d127b21b3a3ce7f7880990e1155fd0ec14a12
shashank kumar <shashankmishra00026@gmail.com>
2025-12-18 21:59:55 +0530

    fixed some issues after deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
+import { Suspense } from "react";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import CanvasLoader from "./components/Loader";
 
 const App = () => {
   return (
@@ -11,7 +13,9 @@ const App = () => {
       {/* this div is for navbar and hero section, enclosed within it. */}
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
+          <Suspense fallback={<CanvasLoader />}>
           <Hero />
+          </Suspense>
         </div>
         {/* just below this section we have, we have other sections */}
 
